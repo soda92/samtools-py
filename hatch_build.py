03a457e4c229f8c573f5c202ee29f53542bc3c7d
Maple <wjxa20152015@gmail.com>
2025-04-23 07:44:20 +0800

    init package

---
+++ hatch_build.py
@@ -0,0 +1,37 @@
+import contextlib
+from typing import Any
+import subprocess
+import shutil
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+@contextlib.contextmanager
+def CD(d: str):
+    import os
+
+    old = os.getcwd()
+    os.chdir(d)
+    yield
+    os.chdir(old)
+
+
+def build():
+    # with CD("diskstat"):
+    #     subprocess.run("make", shell=True, check=True)
+    pass
+
+class CustomBuilder(BuildHookInterface):
+    def initialize(
+        self,
+        version: str,  # noqa: ARG002
+        build_data: dict[str, Any],
+    ) -> None:
+        build_data["tag"] = "py3-none-win_amd64"
+        if self.target_name == "sdist":
+            return
+        build()
+
+
+if __name__ == "__main__":
+    build()
